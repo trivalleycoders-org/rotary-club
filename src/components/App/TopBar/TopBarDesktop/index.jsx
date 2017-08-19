539a8690f0e2cb731db204558ef59fde8b6250fe
klequis <klequis@gmail.com>
2017-08-18 20:54:11 -0700

    started implementing css-modules

--- src/components/App/TopBar/TopBarDesktop/index.jsx
+++ src/components/App/TopBar/TopBarDesktop/index.jsx
@@ -1,26 +1,31 @@
 // TopBarDesktop
 import React from 'react'
+import styles from './style.css'
+import classNames from 'classnames'
 
 const TopBarDesktop = (props) => {
 
   const { brandName, logo, brandColor } = props.brand
-
-  const renderMenuItems = props.menuItems.map((m) => (
-    <a href={m.href} key={m.label} className='menu-item desktop-menu-item'>{m.label}</a>
-  ))
-
   const configStyles = {
     color: brandColor,
     // fontSize: brandSizeDesktop,
   }
+  const imgStyles = classNames({
+    [styles.tvcLogo]: true,
+    [styles.imgResponsive]: true,
+  })
+  
+  const renderMenuItems = props.menuItems.map((m) => (
+    <a href={m.href} key={m.label} className={styles.desktopMenuItem}>{m.label}</a>
+  ))
 
   return (
-    <div className='top-bar'>
-      <div className='branding-desktop'>
-        <img className='tvc-logo img-responsive' src={logo} alt='tvc-logo' />
-        <div className='brand brand-desktop' style={configStyles}>{brandName}</div>
+    <div className={styles.topBar}>
+      <div className={styles.brandingDesktop}>
+        <img className={imgStyles} src={logo} alt='tvc-logo' />
+        <div className={styles.brand} style={configStyles}>{brandName}</div>
       </div>
-      <div className='desktop-menu-items'>
+      <div className={styles.desktopMenuItems}>
         {renderMenuItems}
       </div>
     </div>
