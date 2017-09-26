5d5d498074c6e9b479db3bf959cdccc0d6018897
Jim X. Lin <miljinx@gmail.com>
2017-09-26 10:40:38 -0700

    Re-order table creation in sql script

--- doc/rotary_db_table_creation.sql
+++ doc/rotary_db_table_creation.sql
@@ -5,40 +5,6 @@
 -- Server version	5.7.19
 
 --
--- Table structure for table `Exclusion`
---
-
-DROP TABLE IF EXISTS `Exclusion`;
-CREATE TABLE `Exclusion` (
-  `exclusion_id` int(11) NOT NULL AUTO_INCREMENT,
-  `member_id` int(11) DEFAULT NULL,
-  `role_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`exclusion_id`),
-  KEY `member_id_idx` (`member_id`),
-  KEY `exclusion_role_id_idx` (`role_id`),
-  CONSTRAINT `exclusion_member_id` FOREIGN KEY (`member_id`) REFERENCES `Member` (`member_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `exclusion_role_id` FOREIGN KEY (`role_id`) REFERENCES `Role` (`role_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
---
--- Table structure for table `History`
---
-
-DROP TABLE IF EXISTS `History`;
-CREATE TABLE `History` (
-  `history_id` int(11) NOT NULL AUTO_INCREMENT,
-  `Date` varchar(45) DEFAULT NULL,
-  `member_id` int(11) DEFAULT NULL,
-  `role_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`history_id`),
-  KEY `member_id_idx` (`member_id`),
-  KEY `role_id_idx` (`role_id`),
-  CONSTRAINT `history_member_id` FOREIGN KEY (`member_id`) REFERENCES `Member` (`member_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `history_role_id` FOREIGN KEY (`role_id`) REFERENCES `Role` (`role_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
 -- Table structure for table `Member`
 --
 
@@ -63,4 +29,37 @@ CREATE TABLE `Role` (
   `role_id` int(11) NOT NULL AUTO_INCREMENT,
   `Title` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`role_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Table structure for table `Exclusion`
+--
+
+DROP TABLE IF EXISTS `Exclusion`;
+CREATE TABLE `Exclusion` (
+  `exclusion_id` int(11) NOT NULL AUTO_INCREMENT,
+  `member_id` int(11) DEFAULT NULL,
+  `role_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`exclusion_id`),
+  KEY `member_id_idx` (`member_id`),
+  KEY `exclusion_role_id_idx` (`role_id`),
+  CONSTRAINT `exclusion_member_id` FOREIGN KEY (`member_id`) REFERENCES `Member` (`member_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `exclusion_role_id` FOREIGN KEY (`role_id`) REFERENCES `Role` (`role_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Table structure for table `History`
+--
+
+DROP TABLE IF EXISTS `History`;
+CREATE TABLE `History` (
+  `history_id` int(11) NOT NULL AUTO_INCREMENT,
+  `Date` varchar(45) DEFAULT NULL,
+  `member_id` int(11) DEFAULT NULL,
+  `role_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`history_id`),
+  KEY `member_id_idx` (`member_id`),
+  KEY `role_id_idx` (`role_id`),
+  CONSTRAINT `history_member_id` FOREIGN KEY (`member_id`) REFERENCES `Member` (`member_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `history_role_id` FOREIGN KEY (`role_id`) REFERENCES `Role` (`role_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
