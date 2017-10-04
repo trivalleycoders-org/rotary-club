92856f110fdba8519e02be00244044f56988754b
klequis <klequis@gmail.com>
2017-10-04 10:03:39 -0700

    created new folder structure and added queries

---
+++ doc/db/sql/schedule.next-six.sql
@@ -0,0 +1,5 @@
+select h.history_id, h.date, h.member_id, h.role_id, h.served, m.first_name, r.title
+from history as h, members as m, roles as r
+where h.member_id = m.member_id and h.role_id = r.role_id
+order by date desc
+limit 6;
